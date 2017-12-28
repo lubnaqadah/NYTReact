f86523f9c06494b1b32ec68862077a365408fa05
lubna alqadah <lubna alqadah>
2017-12-28 14:47:34 -0800

    config mongo-uri

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.Promise = global.Promise;
 const MONGODB_URI = "mongodb://heroku_0c2p948l:ajo1bd1qt6v27ljq26cqqt3lad@ds135757.mlab.com:35757/heroku_0c2p948l";
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/nytarticles",
+  MONGODB_URI || "mongodb://localhost/nytarticles",
   {
     useMongoClient: true
   }
